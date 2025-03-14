93369f03ca41e5caf8d8855dc8d14ea1bb07b061
Meiyi <ant19950601@gmail.com>
2025-03-14 20:28:07 +1100

    :memo: Add Start page

--- src/page/Start/index.tsx
+++ src/page/Start/index.tsx
@@ -1,8 +1,15 @@
-const Start = () => (
-  <main className="start-page">
-    <h2>冥想問題後按下開始</h2>
-    <button type="button">開始</button>
-  </main>
-)
+import { useContext } from 'react'
+
+import PageContext from '../../context/Page'
+
+const Start = () => {
+  const { changePage } = useContext(PageContext)
+  return (
+    <main className="start-page">
+      <h2>冥想問題後按下開始</h2>
+      <button type="button" onClick={changePage}>開始</button>
+    </main>
+  )
+}
 
 export default Start
