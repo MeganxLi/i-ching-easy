024408d474507d0e78cf8fd83f03e7d53c96512d
Meiyi <ant19950601@gmail.com>
2025-03-15 23:29:36 +1100

    :memo: Add change page function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import Start from './page/Start'
+import { useMemo, useState } from 'react'
 
-const App = () => (
-  <>
-    {/* <img src="images/coin.png" className="logo react" alt="React logo" /> */}
-    <Start />
-  </>
-)
+import PageSetting from './components/PageSetting'
+import PageContext from './context/Page'
+
+const App = () => {
+  const [page, setPage] = useState<number>(1)
+  const changePage = () => setPage((prevPage) => prevPage + 1)
+
+  const contextValue = useMemo(() => ({ changePage }), [changePage])
+
+  return (
+    <PageContext.Provider value={contextValue}>
+      {PageSetting[page]}
+    </PageContext.Provider>
+  )
+}
 
 export default App
