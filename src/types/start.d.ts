8d972dd4a43c4e2e39b0a8a703af3359acae044c
Meiyi <ant19950601@gmail.com>
2025-08-20 16:01:53 +0800

    :label: Add Hexagram typescript

--- src/types/start.d.ts
+++ src/types/start.d.ts
@@ -1,3 +1,16 @@
 type PageContextType = {
   changePage: () => void
 };
+
+type HexagramType = {
+  id: number,
+  name: string,
+  judgment: string,
+  binary: string
+}
+
+type GetHexagramType = {
+  initial: HexagramType | undefined,
+  changing: HexagramType | undefined,
+  hasChanging: boolean
+}
