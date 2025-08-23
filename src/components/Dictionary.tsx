d94ef13d1d53e8bef595254c2eb775564d4cbde4
Meiyi <ant19950601@gmail.com>
2025-08-23 17:39:59 +0800

    :memo: Update dictionary list and annotation

--- src/components/Dictionary.tsx
+++ src/components/Dictionary.tsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 
 import {
-  BookOpen, Search, X,
+  BookOpen, Eye, Search, X,
 } from 'lucide-react'
+
+import hexagramsData from '../constants/hexagrams.json'
+import { showLineClassName } from '../utils/function'
 
 const Dictionary = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [searchText, setSearchText] = useState<string>('')
+  const [selectedIndex, setSelectedIndex] = useState<number>(1)
 
   const clickDialog = () => setIsOpen(!isOpen)
   const getSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value)
+
+  const findIndex = (
+    no: number,
+  ): HexagramType | undefined => hexagramsData.find((g) => g.id === no)
 
   return (
     <>
@@ -30,7 +38,55 @@ const Dictionary = () => {
               {searchText !== '' && <X onClick={() => setSearchText('')} />}
             </span>
           </div>
+          <div className="dictionary-card">
+            <h5 className="dictionary-title">{findIndex(selectedIndex)?.name}</h5>
+            <span className="dictionary-subtitle">
+              第
+              {findIndex(selectedIndex)?.id}
+              卦
+            </span>
+            <div className="symbol-list">
+              {findIndex(selectedIndex)?.binary.split('').map((item, i) => (
+                <div key={i} className={`symbol-item ${showLineClassName((item))}`}>
+                  <span />
+                  <span />
+                </div>
+              ))}
+            </div>
+            <div className="dictionary-trigram">
+              <p>
+                {`上 ${findIndex(selectedIndex)?.upper}`}
+              </p>
+              <p>
+                {`下 ${findIndex(selectedIndex)?.lower}`}
+              </p>
+            </div>
+            <div className="dictionary-judgment-box">
+              <p className="dictionary-judgment-title">
+                <Eye size={16} />
+                卦辭：
+              </p>
+              <p className="dictionary-judgment-content">
+                {findIndex(selectedIndex)?.judgment}
+              </p>
+            </div>
 
+          </div>
+          <div className="dictionary-list">
+            <h5 className="dictionary-title">
+              卦象列表 (
+              {hexagramsData.length}
+              )
+            </h5>
+            <div className="dictionary-list-content">
+              {hexagramsData.map((item: HexagramType, key) => (
+                <div className={`dictionary-list-item ${selectedIndex === item.id ? 'clicked' : ''}`} key={key} onClick={() => setSelectedIndex(item.id)}>
+                  <span className="dictionary-item-index">{String(item.id).padStart(2, '0')}</span>
+                  <span className="dictionary-item-name">{item.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
